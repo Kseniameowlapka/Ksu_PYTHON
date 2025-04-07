6a330b7eba691bc1afee220417778171c150f284
Kseniameowlapka <kseniabutieva280@gmail.com>
2025-04-07 17:30:16 +0300

    PZ9

--- PZ9/PZ9.py
+++ PZ9/PZ9.py
@@ -7,17 +7,17 @@ def sum_h(a):
 
   if len(a) % 2 != 0:
     print("Словарь должен быть с четным количеством элементов")
-    return None  # Возвращаем None, чтобы указать на ошибку
+    return None
 
-  mid = len(a) // 2  # Находим середину
-  vals = list(a.values())  # Получаем список значений
-  return sum(vals[:mid]), sum(vals[mid:]) # возвращаем сумму половин
+  mid = len(a) // 2  
+  vals = list(a.values()) 
+  return sum(vals[:mid]), sum(vals[mid:])
 
 
 my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 result = sum_h(my_dict)
 
-if result:  # Проверяем, что результат не None (т.е. словарь четный)
+if result: 
   first_half, second_half = result
   print("Сумма первой половины:", first_half)
   print("Сумма второй половины:", second_half)
