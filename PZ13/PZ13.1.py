af78b8ec9709dce53fddb1f58fef83e825394861
Kseniameowlapka <kseniabutieva280@gmail.com>
2025-06-24 11:37:30 +0300

    PZ13

--- PZ13/PZ13.1.py
+++ PZ13/PZ13.1.py
@@ -2,14 +2,16 @@
 #раза.
 import random
 
-def modify_matrix(m):
-    return [[x*2 if i!=j else x for j,x in enumerate(row)] for i,row in enumerate(m)]
+import random
 
-n = 3
-matrix = [[random.randint(1,10) for _ in range(n)] for _ in range(n)]
+M = [[random.randint(-10, 10) for _ in range(3)] for _ in range(3)]
 
-print("Исходная:")
-[print(row) for row in matrix]
+print("Сгенерированная матрица:")
+for row in M:
+    print(row)
+
+M = list(map(lambda i_row: list(map(lambda j_x: j_x[1]*2 if i_row[0]!=j_x[0] else j_x[1], enumerate(i_row[1]))), enumerate(M)))
 
 print("\nИзменённая:")
-[print(row) for row in modify_matrix(matrix)]
+for row in M:
+    print(row)
